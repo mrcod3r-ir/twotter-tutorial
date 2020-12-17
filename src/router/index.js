d2f9a683b4ea94b6fa6699b9ae287c1c4b73e824
mrcod3r.ir <mrcod3r.ir@gmail.com>
2020-12-17 09:40:54 +0330

    complete course

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import UserProfile from '../views/UserProfile'
+import Admin from '../views/Admin';
 
 const routes = [
   {
@@ -12,12 +13,31 @@ const routes = [
     path: '/user/:userId',
     name: 'UserProfile',
     component: UserProfile
+  },
+  {
+    path:'/admin',
+    name:'Admin',
+    component: Admin,
+    meta : {
+      requiresAdmin : true
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach(async(to,from,next)=>{
+
+  // run below before each routing
+  const isAdmin = false;
+  const requiresAdmin = to.matched.some(record => record.meta.requiresAdmin);
+
+  // redirect to home if user is not admin
+  if(requiresAdmin && !isAdmin) next({name:'Home'});
+  else next();
 })
 
 export default router
